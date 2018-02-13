df55170081d7ba4da62326df261c6f62478e299e
Hassan <noueilaty@gmail.com>
2018-02-13 10:28:57 -0600

    Completed fizbuzz exercise

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,28 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (var i = 1; i <= n; i++) {
+    // If i is multiple of 3 and 5, print 'fizzbuzz',
+    // If i is multiple of 3, print 'fizz',
+    // If i is multiple of 5, print 'buzz',
+    // If i is a multiple of neither, then print 'i'
+
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 == 0) {
+      console.log('fizz')
+    } else if (i % 5 == 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+function fizzBuzz(n) {
+
+}
+
 
 module.exports = fizzBuzz;
