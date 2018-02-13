66557ed3898aeaacf23ecb6c9168b0b2b5245d4a
Hassan <noueilaty@gmail.com>
2018-02-13 10:03:44 -0600

    Completed maxchar exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Solution #1: Using forOf
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  // Iterating over a string:
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  // Iterating over an object:
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
